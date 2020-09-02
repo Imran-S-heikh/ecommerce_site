acde3e484ee5a29df5e9101204910a0f5aedb231
Imran-S-heikh <imransheikhsadi@gmail.com>
2020-09-02 22:28:20 +0600

    update

--- src/request/request.js
+++ src/request/request.js
@@ -2,7 +2,8 @@ import Axios from "axios";
 
 async function request(data,url,method = 'POST') {
     return Axios({
-        method,url:`${process.env.REACT_APP_BASE_URL}${url}`,
+        method,
+        url:`${process.env.REACT_APP_BASE_URL}${url}`,
         data: data,
         withCredentials: true,
         headers: {

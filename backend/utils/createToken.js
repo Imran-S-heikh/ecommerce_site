f84e2153027bf7a5a3c9fd4cad0c68407b92a17a
Imran-S-heikh <imransheikhsadi@gmail.com>
2020-09-02 21:52:21 +0600

    backend removed

--- backend/utils/createToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-function createToken(id) {
-    return jwt.sign({id},process.env.JWT_SECRET,{
-        expiresIn: process.env.JWT_EXPIRE_IN
-    })
-}
-
-module.exports = createToken;
